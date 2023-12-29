835008608d594f9e372ea4712732e2e8a4b035bd
KangWookLim <lakumwa99@gmail.com>
2023-12-29 15:13:12 +0900

    d

--- src/main/java/com/example/trelloweb/login/controller/LoginController.java
+++ src/main/java/com/example/trelloweb/login/controller/LoginController.java
@@ -1,45 +1,20 @@
 package com.example.trelloweb.login.controller;
 
-import com.example.trelloweb.login.service.LoginService;
-import com.example.trelloweb.login.vo.GoogleInfResponse;
-import com.example.trelloweb.login.vo.GoogleRequest;
-import com.example.trelloweb.login.vo.GoogleResponse;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.util.HashMap;
-import java.util.Map;
-
-//@CrossOrigin("*")
-//@Controller
-//public class LoginController {
-//
-//    @GetMapping("/login")
-//    public ModelAndView loginPage() {
-//        ModelAndView view = new ModelAndView();
-//        view.setViewName("views/login");
-//        return view;
-//    }
-//
-//
-//}
-
-@RestController
-@RequestMapping(value = "/login/oauth2", produces = "application/json")
+@CrossOrigin("*")
+@Controller
 public class LoginController {
 
-    LoginService loginService;
-
-    public LoginController(LoginService loginService) {
-        this.loginService = loginService;
+    @GetMapping("/login")
+    public ModelAndView loginPage() {
+        ModelAndView view = new ModelAndView();
+        view.setViewName("views/login");
+        return view;
     }
 
-    @GetMapping("/code/{registrationId}")
-    public void googleLogin(@RequestParam String code, @PathVariable String registrationId) {
-        loginService.socialLogin(code, registrationId);
-    }
+
 }
