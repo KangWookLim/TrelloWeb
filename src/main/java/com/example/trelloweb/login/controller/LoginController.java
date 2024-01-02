543ed1cef5b0a1fa80f24c917769cd799b486778
KangWookLim <lakumwa99@gmail.com>
2024-01-02 09:23:08 +0900

    asdf

--- src/main/java/com/example/trelloweb/login/controller/LoginController.java
+++ src/main/java/com/example/trelloweb/login/controller/LoginController.java
@@ -4,17 +4,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
-
-@CrossOrigin("*")
-@Controller
-public class LoginController {
-
-    @GetMapping("/login")
-    public ModelAndView loginPage() {
-        ModelAndView view = new ModelAndView();
-        view.setViewName("views/login");
-        return view;
-    }
-
-
-}
+//
+//@CrossOrigin("*")
+//@Controller
+//public class LoginController {
+//
+//    @GetMapping("/login")
+//    public ModelAndView loginPage() {
+//        ModelAndView view = new ModelAndView();
+//        view.setViewName("views/login");
+//        return view;
+//    }
+//
+//
+//}
