d48d819c58553aee1e081b8b18033447397d92ba
jo_sung <zounsam@naver.com>
2024-01-03 15:51:19 +0900

    check

---
+++ src/main/resources/static/js/board-detail/boardsidebar.js
@@ -0,0 +1,5 @@
+$("#side-earase-btn").click(function(){
+    $(".sidebar-container").removeClass("sidebar-show")
+    $(".boardsidebar").css("width", 16);
+    $(".sidebar-collapse-container").addClass("sidebar-show")
+})
