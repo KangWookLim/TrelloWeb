79b602aa3313f8c0777630b1756a8ce0252c5035
jo_sung <zounsam@naver.com>
2024-01-30 17:55:38 +0900

    profile

--- src/main/java/com/example/trelloweb/user/Recent_Act/repo/ActJPARepo.java
+++ src/main/java/com/example/trelloweb/user/Recent_Act/repo/ActJPARepo.java
@@ -1,9 +1,17 @@
 package com.example.trelloweb.user.Recent_Act.repo;
 
 import com.example.trelloweb.user.Recent_Act.entity.RecentActVo;
+import com.example.trelloweb.user.base.entity.UserVo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
+@EnableJpaRepositories
 public interface ActJPARepo extends JpaRepository<RecentActVo,Long> {
+    @Query("select (count(u) > 0) from User_Recent_Act u where u.actUri = :actUri and u.uservo = :userVo")
+    boolean existsByActUriAndUservo(@Param("actUri") String actUri, @Param("userVo") UserVo userVo);
+    RecentActVo findByActUriAndUservo(String actUri, UserVo userVo);
 }
--- src/main/java/com/example/trelloweb/user/Recent_Act/service/ActService.java
+++ src/main/java/com/example/trelloweb/user/Recent_Act/service/ActService.java
@@ -3,9 +3,12 @@ package com.example.trelloweb.user.Recent_Act.service;
 
 import com.example.trelloweb.user.Recent_Act.repo.ActJPARepo;
 import com.example.trelloweb.user.Recent_Act.entity.RecentActVo;
+import com.example.trelloweb.user.base.entity.UserVo;
 import com.example.trelloweb.user.base.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 @Service
 @RequiredArgsConstructor
@@ -13,10 +16,24 @@ public class ActService {
     private final ActJPARepo actJPRepo;
     private final UserService userService;
 
-    public void create(String useruid, String category){
+    public void create(String useruid, String uri){
         RecentActVo recentActVo = new RecentActVo();
         recentActVo.setUservo(userService.findByUSER_UID(useruid).orElse(null));
-        recentActVo.setActCategory(category);
+        recentActVo.setActUri(uri);
         actJPRepo.save(recentActVo);
+    }
+
+    public void addRecentAct(String useruid, String actUri){
+        UserVo user = userService.findByUSER_UID(useruid).orElse(null);
+        if(actJPRepo.existsByActUriAndUservo(actUri,user)){
+            RecentActVo recentAct = actJPRepo.findByActUriAndUservo(actUri,user);
+            recentAct.setActDate(LocalDateTime.now());
+            actJPRepo.save(recentAct);
+        }else{
+            RecentActVo recentAct = new RecentActVo();
+            recentAct.setActUri(actUri);
+            recentAct.setUservo(user);
+            actJPRepo.save(recentAct);
+        }
     }
 }
