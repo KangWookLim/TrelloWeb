f8dc69b15a37d5a8a17b6a3a48710ac76207722d
KangWookLim <lakumwa99@gmail.com>
2024-01-30 14:51:10 +0900

    asfd

--- src/main/java/com/example/trelloweb/user/Starred_Board/entity/StarredBoardVo.java
+++ src/main/java/com/example/trelloweb/user/Starred_Board/entity/StarredBoardVo.java
@@ -1,28 +1,28 @@
-//package com.example.trelloweb.user.Starred_Board.entity;
-//
-//import com.example.trelloweb.board.Base.entity.BoardVo;
-//import com.example.trelloweb.user.base.entity.UserVo;
-//import com.fasterxml.jackson.annotation.JsonBackReference;
-//import jakarta.persistence.*;
-//import lombok.AllArgsConstructor;
-//import lombok.Data;
-//import lombok.RequiredArgsConstructor;
-//
-//@IdClass(StarredBoardPK.class)
-//@Data
-//@AllArgsConstructor
-//@RequiredArgsConstructor
-//@Entity(name = "User_Starred_Board")
-//public class StarredBoardVo {
-//    @Id
-//    @ManyToOne
-//    @JoinColumn(name = "user_uid")
-//    @JsonBackReference
-//    private UserVo uservo;
-//
-//    @Id
-//    @ManyToOne
-//    @JoinColumn(name = "Starred_Board_ID")
-//    @JsonBackReference
-//    private BoardVo boardVo;
-//}
+package com.example.trelloweb.user.Starred_Board.entity;
+
+import com.example.trelloweb.board.Base.entity.BoardVo;
+import com.example.trelloweb.user.base.entity.UserVo;
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@IdClass(StarredBoardPK.class)
+@Data
+@AllArgsConstructor
+@RequiredArgsConstructor
+@Entity(name = "User_Starred_Board")
+public class StarredBoardVo {
+    @Id
+    @ManyToOne
+    @JoinColumn(name = "user_uid")
+    @JsonBackReference
+    private UserVo uservo;
+
+    @Id
+    @ManyToOne
+    @JoinColumn(name = "Starred_Board_ID")
+    @JsonBackReference
+    private BoardVo boardVo;
+}
